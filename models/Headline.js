30e6855c67c6028e5fcb11552554a1b08859d230
unknown <mlinback83@gmail.com>
2019-03-26 16:50:23 -0600

    updated headline.js

--- models/Headline.js
+++ models/Headline.js
@@ -17,13 +17,6 @@ var headlineSchema = new Schema({
     type: Boolean,
     default: false
   }
-//   // `note` is an object that stores a Note id
-//   // The ref property links the ObjectId to the Note model
-//   // This allows us to populate the headline with an associated Note
-//   note: {
-//     type: Schema.Types.ObjectId,
-//     ref: "Note"
-//   }
 });
 
 var Headline = mongoose.model("Headline", headlineSchema);
