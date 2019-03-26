e92f6db6f11443b301746ca31724dbd31dd1b3f9
unknown <mlinback83@gmail.com>
2019-03-26 15:45:23 -0600

    fixes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ require("./config/routes")(router);
 app.use(express.static(__dirname + "/public"));
 
 // Connect Handlebars to Express app
-app.connect("handlebars", expressHandlebars({
+app.engine("handlebars", expressHandlebars({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
